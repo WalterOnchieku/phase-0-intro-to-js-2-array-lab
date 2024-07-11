0c8c7c1f770a5110cde84695b2f454451d8ec92a
root <root@DESKTOP-RHA5CJR.localdomain>
2024-07-11 05:22:39 -0700

    array methods

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+cats.push ("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+ cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    let cats2 = cats.slice();
+    cats2.push("Broom");
+    return cats2;
+}
+
+function prependCat(name){
+    let cats2 = cats.slice();
+    cats2.unshift("Arnold");
+    return cats2;
+}
+
+function removeLastCat(){
+    let cats2 = cats.slice();
+    cats2.pop();
+    return cats2;
+
+}
+
+function removeFirstCat(){
+    let cats2 = cats.slice();
+    cats2.shift();
+    return cats2;
+}
